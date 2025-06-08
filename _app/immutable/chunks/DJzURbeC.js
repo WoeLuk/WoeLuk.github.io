fb409a03266f0fd33ddac12c9442c55d7e425698
LukasSpielt <lukaswoesenboeck@gmail.com>
2025-06-08 23:51:09 +0200

    Implemented real F1 font

--- _app/immutable/chunks/DJzURbeC.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1t4oc5a)==null?void 0:s.base)??"/WoeLuk.github.io";var t;const e=((t=globalThis.__sveltekit_1t4oc5a)==null?void 0:t.assets)??a;export{e as a,a as b};
