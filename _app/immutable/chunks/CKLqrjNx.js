50e9d171e300ffbcf42687ec364b5798c3a73c51
Lukas <WoesenboeckL21085@sus.htl-grieskirchen.at>
2026-02-28 11:56:25 +0100

    Added backend connection to community page

--- _app/immutable/chunks/CKLqrjNx.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./Crv4zYnq.js";a();
--- _app/immutable/chunks/Dm3lehDp.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1tkp5mj)==null?void 0:s.base)??"/WoeLuk.github.io";var t;const a=((t=globalThis.__sveltekit_1tkp5mj)==null?void 0:t.assets)??e;export{a,e as b};
